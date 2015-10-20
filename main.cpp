1b1ae4c9ac23f0012d7f1859c136d81a73505620
Kaj-Michael Lang <milang@tal.org>
2015-10-20 12:23:24 +0300

    Add tesser to qml. Add simple filesystem model to qml.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,30 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include <QtQml>
+#include <QFileSystemModel>
+#include <QtGui/QDesktopServices>
+#include <QStandardPaths>
 #include "tesser.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-
+    QApplication app(argc, argv);    
     QQmlApplicationEngine engine;
+    QString p;
+
+    qmlRegisterType<tesser>("org.tal.tesser", 1, 0, "TesseractOCR");
+
+    QFileSystemModel *fsm = new QFileSystemModel(&engine);
+    p=QStandardPaths::writableLocation(QStandardPaths::PicturesLocation);
+    fsm->setRootPath(p);
+    fsm->setResolveSymlinks(true);
+
+    engine.rootContext()->setContextProperty("filePicturesPath", p);
+    engine.rootContext()->setContextProperty("fileSystemModel", fsm);
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
-    tesser *t=new tesser();
-    t->ocr();
+    //tesser *t=new tesser();
+    //t->ocr("/home/milang/ocrtest.png");
 
     return app.exec();
 }
